7486c76aacb2f59a054a0ed4ddab7e444fc58f22
chuanlon <chuanlon@buffalo.edu>
2023-02-21 22:52:52 -0500

    socket set up

--- chuanlon/src/chuanlon_assignment1.c
+++ chuanlon/src/chuanlon_assignment1.c
@@ -20,8 +20,11 @@
  *
  * This contains the main function. Add further description here....
  */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+#include <winsock2.h>
 
 #include "../include/global.h"
 #include "../include/logger.h"
@@ -35,11 +38,41 @@
  */
 int main(int argc, char **argv)
 {
+
 	/*Init. Logger*/
 	cse4589_init_log(argv[2]);
 
 	/*Clear LOGFILE*/
 	fclose(fopen(LOGFILE, "w"));
+
+    if (argc < 3 ||strcmp(argv[1],"c") != 0 || strcmp(argv[1],"s") != 0 ) {
+        return -1; // 代表当前指令少于3个
+    }
+    struct sockaddr_in server_addr;
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);//创建套接字
+    if(server_socket < 0) {
+        perror("socket");
+        return -1;//创建失败的错误处理
+    }
+    memset(&server_addr,0,sizeof (server_addr));
+    char* port = argv[2];
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(port));
+
+    printf("Port: %d:", ntohs(server_addr.sin_port));
+
+    if (bind(server_socket,(struct  sockaddr *)&server_addr, sizeof server_addr) < 0){
+        perror("socket");
+        return -1;//创建失败的错误处理
+    }
+
+    if (listen(server_socket,3) < 0){
+        perror("socket");
+        return -1;//创建失败的错误处理
+    }
+
 
 	/*Start Here*/
 
