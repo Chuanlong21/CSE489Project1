9367e6a9b6d219ed5487fed4d18ad566fd9ba89a
fhuang566 <fhuang566@gmail.com>
2023-02-27 02:04:57 -0500

    s

--- chuanlon/src/common.c
+++ chuanlon/src/common.c
@@ -92,7 +92,7 @@ void listing(int* connectd, int count){
         int e;
         if (getpeername(fd, (struct sockaddr *)&client_addr, &len) == 0){
             printf("getpeername success\n");
-            e = getnameinfo((struct sockaddr *)&client_addr, sizeof(client_addr), hostname, sizeof(hostname), serv, sizeof(serv), 0);
+            e = getnameinfo(client_addr, len, hostname, sizeof(hostname), serv, sizeof(serv), 0);
             if( e == 0){
                 printf("getnameinfo success\n");
                 //printf("Socket FD %i is connected to a peer at IP address %s\n", fd, inet_ntoa(client_addr.sin_addr));
