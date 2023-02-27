af4ab68319d0e319cd29f6710198d44aa3ef7134
fhuang566 <fhuang566@gmail.com>
2023-02-26 23:17:04 -0500

    s

--- chuanlon/src/common.c
+++ chuanlon/src/common.c
@@ -63,12 +63,13 @@ void show_Author(){
 void list_client(fd_set *clients, int max_fd){
     printf("max_fd: ", max_fd);
     for (int e=0; e <= max_fd; e++){
-        struct sockaddr_in client_addr;
-        socklen_t len;
-        if (FD_ISSET(e, &clients)){
+ 
+        if (FD_ISSET(e, clients)){
+            struct sockaddr_in client_addr;
+            socklen_t len;
             if (getpeername(e, (struct sockaddr *)&client_addr, &len) == 0){
 //              printf("%-5d%-35s%-20s%-8d\n", e, hostname, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port))
-                printf("IP: ", e, inet_ntoa(client_addr.sin_addr));
+                printf("Socket FD %i is connected to a peer at IP address %s\n", e, inet_ntoa(client_addr.sin_addr));
             }else{
                 perror("getpeername");
             }
