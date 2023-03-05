9c41e1cb8307903b2ea998c9061f04e1f1f6dc8b
chuanlon <chuanlon@buffalo.edu>
2023-03-04 22:54:33 -0500

    connect to server update

--- chuanlon/src/client.c
+++ chuanlon/src/client.c
@@ -33,6 +33,7 @@
 #include <sys/types.h>
 #include <netdb.h>
 #include <sys/select.h>
+#include <unistd.h>
 
 #include "../include/common.h"
 #include "../include/logger.h"
@@ -44,7 +45,7 @@
 #define CMD_SIZE 100
 
 
-int connect_to_host(char *server_ip, char *server_port);
+int connect_to_host(char *server_ip, char *server_port,  char* port);
 
 /**
 * main function
@@ -63,29 +64,32 @@ int c_startUp(char *port)
 //        exit(-1);
 //    }
 
-    int client_socket,head_socket, selret, sock_index, connection;
+    int client_socket,head_socket, selret, sock_index;
     int server = -1;
     fd_set master_list, watch_list;
-    struct sockaddr_in addr;
-    char ip_buff[16];
+    struct addrinfo hints, *res;
+    char hostName[1024];
 
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("8.8.8.8");
-    addr.sin_port = htons(53);
+    /* Set up hints structure */
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    gethostname(hostName, 1024);
+    /* Fill up address structures */
+    if (getaddrinfo(hostName, NULL, &hints, &res) != 0)
+        perror("getaddrinfo failed");
 
-    //create socket and connect to the addr for IP address
-    client_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if(client_socket < 0){
-        error("IP");
-    }
-    connection = connect(client_socket, (struct sockaddr*) &addr,sizeof (addr));
+    /* Socket */
+    client_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (client_socket < 0)
+        perror("Cannot create socket");
 
-    if (connection < 0){
-        error("IP");
-    }
-    socklen_t len = sizeof(addr);
-    getsockname(client_socket, (struct sockaddr*) &addr, &len);
-    inet_ntop(AF_INET, &addr.sin_addr, ip_buff, 16);
+    /* Bind */
+    if (bind(client_socket, res->ai_addr, res->ai_addrlen) < 0)
+        perror("Bind failed");
+
+    freeaddrinfo(res);
 
     /* Zero select FD sets */
     FD_ZERO(&master_list);
@@ -136,7 +140,7 @@ int c_startUp(char *port)
                                 rev[2][strlen(rev[2])  -1 ] = 0;
                                 if (IPv4_verify(rev[1]) == 1 && validNumber(rev[2]) == 1){
                                     //when we have login command, we will need to connect to the host sever
-                                    server = connect_to_host(rev[1], rev[2]);
+                                    server = connect_to_host(rev[1], rev[2], port);
                                     if (server < 0){
                                         error("LOGIN");
                                     }
@@ -144,13 +148,12 @@ int c_startUp(char *port)
                             } else error("LOGIN");
 
                         }else if(strcmp("PORT\n", cmd) == 0){
+
                             show_port(port);
                         }else if (strcmp("AUTHOR\n", cmd) == 0){
                             show_Author();
                         }else if (strcmp("IP\n", cmd) == 0){
-                            cse4589_print_and_log("[%s:SUCCESS]\n", "IP");
-                            cse4589_print_and_log("IP:%s\n", ip_buff);
-                            cse4589_print_and_log("[%s:END]\n", "IP");
+                            show_ip(client_socket);
                         }else if (strcmp("EXIT\n", cmd) == 0){
                             //删除了之后 服务端也要把watch list里它的socket给删除
                             if (server > 0){
@@ -159,9 +162,7 @@ int c_startUp(char *port)
                                 cse4589_print_and_log("[%s:END]\n", "EXIT");
                                 exit(EXIT_SUCCESS);
                             }else error("EXIT");
-
                         }
-
                         free(cmd);
                     }
                 }
@@ -191,15 +192,20 @@ int c_startUp(char *port)
     }
 }
 
-int connect_to_host(char *server_ip, char* server_port)
+int connect_to_host(char *server_ip, char* server_port, char* port)
 {
     int fdsocket;
+    struct sockaddr_in addr;
     struct addrinfo hints, *res;
 
     /* Set up hints structure */
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(atoi(port));
 
     /* Fill up address structures */
     if (getaddrinfo(server_ip, server_port, &hints, &res) != 0)
@@ -209,6 +215,10 @@ int connect_to_host(char *server_ip, char* server_port)
     fdsocket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if(fdsocket < 0)
         perror("Failed to create socket");
+
+    if (bind(fdsocket, (struct sockaddr *)&addr, sizeof(addr)) < 0){
+        perror("Bind fail");
+    }
 
     /* Connect */
     if(connect(fdsocket, res->ai_addr, res->ai_addrlen) < 0)
